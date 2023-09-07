16a8dd8c1e24948c94b29db96ef5347f4bf10524
Shobi <it21821240@my.sliit.lk>
2023-09-07 09:16:42 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 8070;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true
+}))
+
+app.use(express.json({ limit: "20mb", extended: true }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
 const URL = process.env.MONGODB_URL;
 
@@ -19,6 +24,8 @@ mongoose.connect(URL, {
   
 });
 
+mongoose.set('strictQuery', true);
+
 const connection = mongoose.connection;
 connection.once("open", () =>{
     console.log("Mongodb Connection success!");
@@ -27,3 +34,15 @@ connection.once("open", () =>{
 app.listen(PORT, () =>{
     console.log(`Server is running on PORT number: ${PORT}`)
 })
+
+const salesexecutiveRouter = require('./routes/SalesExecutives');
+app.use("/salesexecutive",salesexecutiveRouter);
+
+const deliverydriverRouter = require ('./routes/DeliveryDrivers');
+app.use("/deliverydriver",deliverydriverRouter);
+
+const adminRouter = require ('./routes/Admin');
+app.use("/admin",adminRouter);
+
+const loginRouter = require('./routes/Logins');
+app.use("/login",loginRouter);
