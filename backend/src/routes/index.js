f6d1559d221f260195a2b6a3c04dc3beb58c9961
jeyalan <It21835414@my.sliit.lk>
2023-09-29 15:46:13 +0530

    remove auth module deps

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 
-const auth = require('../routes/auth')
 const supplier = require('../routes/suppliers')
 
 module.exports = function (app) {
     app.use(cors())
     app.use(express.json())
-    app.use('/api/auth', auth)
     app.use('/api/supplier', supplier)
 }
